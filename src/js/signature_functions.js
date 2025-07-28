41afa851f1b4bf52a8ecb8ade07b5e661a6803cf
ARIKBOGA <you@example.com>
2025-07-28 18:10:22 +0300

    Initial commit

---
+++ src/js/signature_functions.js
@@ -0,0 +1,20 @@
+const bilisim = {
+    firstName: "Burak",
+    lastName: "ARIKBOĞA",
+    prefix: "(Mr.)",
+    title: "BT Uzmanı / IT Specialist",
+    company: "YAVUZSAN BRAKE DISC & DRUM & PAD",
+    email: "bilisim@yavuzsan.com",
+    phone: "+90 332 239 22 00",
+    website: "www.yavuzsan.com",
+    address1: "Konya Organize San. Böl. Kuddusi Cad. No:16",
+    address2: "Selçuklu / Konya / Türkiye",
+    facebookURL: "https://www.facebook.com/yavuzsanotomotiv",
+    linkedinURL: "https://www.linkedin.com/company/yavuzsan-otomoti%CC%87v/",
+    twitterURL: "https://x.com/yavuzsan",
+    youtubeURL: "https://www.youtube.com/@yavuzsanotomotiv1",
+    instagramURL: "https://www.instagram.com/yavuzsanotomotiv",
+    //pinterestURL: "https://pinterest.com/yavuzsan",
+    mobile: "+90 332 239 22 00"
+};
+
